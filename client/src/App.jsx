cdaa8e43455b711b93c0332fc8f813709555e2b1
carlosmendoza12 <carlosmendoza120905@gmail.com>
2023-11-23 15:40:47 -0600

    Realice modificacion en el apartado de tienda en la seccion AboutUs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,49 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Tablas from "./components/Tablas";
+import { AuthProvider } from "./context/AuthContext";
+import { CarritoProvider } from "./context/CarritoContext";
+import { DesarrolladorProvider } from "./context/DesarrolladoresContext";
+import { EditoraProvider } from "./context/EditorasContext";
+import { GeneroProvider } from "./context/GenerosContext";
+import { ImagenProvider } from "./context/ImagenesContext";
+import { PlataformaProvider } from "./context/PlataformasContext";
+import { ProductoProvider } from "./context/ProductosContext";
+import { ProductosGenerosProvider } from "./context/ProductosGenerosContext";
+import { RolProvider } from "./context/RolesContext";
+import { UsuarioProvider } from "./context/UsuariosContext";
+import { WishlistProvider } from "./context/WishlistContext";
+import { NoticiaProvider } from "./context/noticiasContext";
+import CUDesarrolladores from "./pages/crud/CUDesarrolladores";
+import CUEditoras from "./pages/crud/CUEditoras";
+import CUGeneros from "./pages/crud/CUGeneros";
+import CUImagenes from "./pages/crud/CUImagenes";
+import CUPlataformas from "./pages/crud/CUPlataformas";
+import CUProductos from "./pages/crud/CUProductos";
+import CUProductosGeneros from "./pages/crud/CUProductosGeneros";
+import CUUsuarios from "./pages/crud/CUUsuarios";
 import CrudLogin from "./pages/crud/CrudLogin";
 import Dashboard from "./pages/crud/Dashboard";
+import Desarrolladores from "./pages/crud/Desarrolladores";
+import Editoras from "./pages/crud/Editoras";
 import Generos from "./pages/crud/Generos";
 import Imagenes from "./pages/crud/Imagenes";
 import Noticias from "./pages/crud/Noticias";
 import PerfilEmpleado from "./pages/crud/PerfilEmpleado";
 import Plataformas from "./pages/crud/Plataformas";
 import Productos from "./pages/crud/Productos";
+import ProductosGeneros from "./pages/crud/ProductosGeneros";
 import Usuarios from "./pages/crud/Usuarios";
 import Ventas from "./pages/crud/Ventas";
-import RutaProtegida from "./RutaProtegida";
 import CUNoticias from "./pages/crud/cuNoticias";
-import { AuthProvider } from "./context/AuthContext";
-import Tablas from "./components/Tablas";
-import { NoticiaProvider } from "./context/noticiasContext";
-import { GeneroProvider } from "./context/GenerosContext";
-import CUGeneros from "./pages/crud/CUGeneros";
-import { PlataformaProvider } from "./context/PlataformasContext";
-import CUPlataformas from "./pages/crud/CUPlataformas";
-import { EditoraProvider } from "./context/EditorasContext";
-import CUEditoras from "./pages/crud/CUEditoras"; 
-import Editoras from "./pages/crud/Editoras";
-import { DesarrolladorProvider } from "./context/DesarrolladoresContext";
-import CUDesarrolladores from "./pages/crud/CUDesarrolladores";
-import Desarrolladores from "./pages/crud/Desarrolladores";
-import { ProductoProvider } from "./context/ProductosContext";
-import CUProductos from "./pages/crud/CUProductos";
-import { ImagenProvider } from "./context/ImagenesContext";
-import CUImagenes from "./pages/crud/CUImagenes";
-import CUUsuarios from "./pages/crud/CUUsuarios";
-import { UsuarioProvider } from "./context/UsuariosContext";
-import { RolProvider } from "./context/RolesContext";
-import ProductosGeneros from "./pages/crud/ProductosGeneros";
-import { ProductosGenerosProvider } from "./context/ProductosGenerosContext";
-import CUProductosGeneros from "./pages/crud/CUProductosGeneros";
-import NoticiasTienda from "./pages/tienda/NoticiasTienda";
-import Catalogo from "./pages/tienda/Catalogo";
-import WishlistPage from "./pages/tienda/Wishlist";
-import { WishlistProvider } from "./context/WishlistContext";
-import { CarritoProvider } from "./context/CarritoContext";
+import AboutUs from "./pages/tienda/AboutUs";
 import CarritoPage from "./pages/tienda/Carrito";
+import Catalogo from "./pages/tienda/Catalogo";
+import NoticiasTienda from "./pages/tienda/NoticiasTienda";
 import ProductoDetalle from "./pages/tienda/Producto";
-import AboutUs from "./pages/tienda/AboutUs";
 import Soporte from "./pages/tienda/Soporte";
-import AboutUs from "./pages/tienda/AboutUs";
-import Soporte from "./pages/tienda/Soporte";
+import WishlistPage from "./pages/tienda/Wishlist";
+
 function App() {
   return (
     <AuthProvider>
@@ -105,9 +103,7 @@ function App() {
             <Route path="/tienda/carrito" element={<CarritoPage />}></Route>
             <Route path="/tienda/catalogo/producto/:id" element={<ProductoDetalle />}></Route>
             <Route path="/tienda/sobrenosotros" element={<AboutUs />}></Route>
-            <Route path="/tienda/soporte" element={<Soporte />}></Route>
-            <Route path="/tienda/sobrenosotros" element={<AboutUs />}></Route>
-<Route path="/tienda/soporte" element={<Soporte />}></Route>
+            <Route path="/tienda/soporte" element={<Soporte />}></Route>
         </Routes>
       </BrowserRouter>
         </CarritoProvider>
--- client/src/pages/tienda/AboutUs.jsx
+++ client/src/pages/tienda/AboutUs.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React from 'react';
 
-function AboutUs() {
-  return <h1>Sobre Nosotros</h1>;
-}
+const AboutUs = () => {
+  return (
+    <div className="text-white font-sans">
+      <div class="bg-gray-800 p-4">
+    <nav class="container mx-auto px-6 py-4">
+    <ul class="flex space-x-8">
+            <li>
+                <a href="/tienda/catalogo" class="text-white hover:text-gray-300">Catálogo</a>
+            </li>
+            <li>
+                <a href="/tienda/sobrenosotros" class="text-white hover:text-gray-300">Sobre Nosotros</a>
+            </li>
+            <li>
+                <a href="/tienda/noticias" class="text-white hover:text-gray-300">Noticias</a>
+            </li>
+            
+            <li>
+                <a href="/tienda/wishlist" class="text-white hover:text-gray-300">Wishlist</a>
+            </li>
+            <li>
+                <a href="/tienda/carrito" class="text-white hover:text-gray-300">Carrito</a>
+            </li>
+            <li>
+                <a href="/tienda/soporte" class="text-white hover:text-gray-300">Soporte</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+      <div className="container mx-auto flex justify-between items-center px-8">
+        <div className="w-1/2">
+          <h3 className="text-white text-xl"></h3>
+          <h1 className="text-purple-600 text-4xl font-bold mt-2">Nuestra Historia</h1>
+          <p className="mt-4 text-lg">
+            Una tienda de videojuegos creada por estudiantes con el fin de fomentar la venta de estos de manera eficiente.
+            Nos esforzamos por ofrecer una amplia gama de títulos y proporcionar un servicio excepcional para nuestros clientes.
+            En nuestra búsqueda por inspirar a los jugadores, hemos creado un espacio donde los sueños se encuentran con la realidad digital,
+            donde cada clic es una nueva aventura y cada título un mundo por descubrir. ¡Bienvenidos a nuestra tienda de sueños y aventuras!
+          </p>
+        </div>
+        <div className="w-1/2 flex justify-end py-10">
+          <img
+            src="../../../upload/imagen.jpg"
+            className="rounded-full border-4 border-purple-600"
+            alt="Imagen de videojuegos"
+            style={{ width: '300px', height: '300px' }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
